692983de321f099f80a07e8e8e3ee4ab40b10f83
Reid Miller <rmiller9@Reids-MacBook-Air.local>
2019-07-09 15:30:04 -0700

    Done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
-
+const db = require('./models');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,8 +9,28 @@ app.use(express.static('static'));
 app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
+app.get('/', function( req, res) {
+  db.widget.findAll().then(function(widgets) {
+    res.render('index', {widgets})
+  })
+});
 
+app.post('/', function(req, res) {
+  db.widget.create({
+    description: req.body.widgetDescription,
+    quantity: req.body.widgetQuantity
+  }).then(function(widgets) {
+    res.redirect('/');
+  });
+})
 
+app.delete('/:id', function(req, res) {
+  db.widget.destroy({
+    where: {id: parseInt(req.params.id)}
+  }).then(function(data) {
+    res.redirect('/');
+  })
+})
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
